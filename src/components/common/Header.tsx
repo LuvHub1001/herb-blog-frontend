b13a82126e6256a3752857c59847e25e0b2a72e9
dy7791 <dy7791@naver.com>
2025-04-01 15:50:25 +0900

    add:: Header

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,14 @@
+import { Navibar } from "..";
+
+function Header() {
+  return (
+    <>
+      <Navibar />
+      <div className="flex h-40 items-center justify-center bg-header text-white">
+        Header
+      </div>
+    </>
+  );
+}
+
+export default Header;
