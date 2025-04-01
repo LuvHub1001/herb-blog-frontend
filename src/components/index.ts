62449c837ea0bf074b2cb7f72dc75d1e3ee946a5
dy7791 <dy7791@naver.com>
2025-04-01 15:51:37 +0900

    code split

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 // common
 import ErrorBoundary from "./common/ErrorBoundary";
 import Loading from "./common/Loading";
+import Navibar from "./common/Navibar";
+import Header from "./common/Header";
+import Footer from "./common/Footer";
 
-export { ErrorBoundary, Loading };
+export { ErrorBoundary, Loading, Navibar, Header, Footer };
