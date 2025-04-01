b1d1f3a0b282dffac3d3db13d1a6cacbc1cf1722
dy7791 <dy7791@naver.com>
2025-04-01 15:50:50 +0900

    fix:: withCommonLayout Style

--- src/hoc/withCommonLayout.tsx
+++ src/hoc/withCommonLayout.tsx
@@ -1,14 +1,19 @@
 import { ComponentType } from "react";
+import { Header, Footer } from "../components";
 
 function withCommonLayout(Component: ComponentType) {
   const WrappedComponent = () => {
     return (
       <div>
-        <div>Header</div>
-        <div>
+        <Header />
+        <div
+          style={{
+            minHeight: "calc(100vh - 360px)",
+          }}
+        >
           <Component />
         </div>
-        <div>Footer</div>
+        <Footer />
       </div>
     );
   };
