01dd6a3769c1ca4824b6a19a1d7ec17061dc89f1
dy7791 <dy7791@naver.com>
2025-04-01 15:50:15 +0900

    fix:: Navibar > sticky

---
+++ src/components/common/Navibar.tsx
@@ -0,0 +1,9 @@
+function Navibar() {
+  return (
+    <div className="flex sticky top-0 z-10 w-screen h-20 bg-black text-white items-center">
+      Navibar
+    </div>
+  );
+}
+
+export default Navibar;
