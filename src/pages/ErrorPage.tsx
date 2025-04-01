1b2b834a471a0e9a8a509028935db885252d7608
dy7791 <dy7791@naver.com>
2025-04-01 14:19:25 +0900

    add:: ErrorPage

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,12 @@
+function ErrorPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-3xl text-center font-bold">
+        <div className="text-3xl">에러가 발생했습니다.</div>
+        <div className="text-3xl">잠시 후 다시 시도해주세요😵</div>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
