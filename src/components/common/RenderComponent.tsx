9680178b5e88757e90dc2524abbc69ccf07b7f83
dy7791 <dy7791@naver.com>
2025-04-07 08:51:38 +0900

    add:: RenderComponent

---
+++ src/components/common/RenderComponent.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface RenderProps<T> {
+  className: string;
+  items: T[];
+  render: (item: T) => ReactNode;
+}
+
+function RenderComponent<T>({ className, items, render }: RenderProps<T>) {
+  return (
+    items && (
+      <div className={className}>
+        {items.map((item: T, idx: number) => {
+          return <div key={idx}>{render(item)}</div>;
+        })}
+      </div>
+    )
+  );
+}
+
+export default RenderComponent;
