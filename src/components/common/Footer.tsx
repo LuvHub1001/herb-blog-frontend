6a3a2e9bfa5351b477756b86d213a09b9b8a9af7
dy7791 <dy7791@naver.com>
2025-04-07 14:40:00 +0900

    fix:: Footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="flex h-30 justify-center items-center bg-black text-white"></div>
+    <div className="flex h-30 mt-30 justify-center items-center bg-black text-white"></div>
   );
 }
 
