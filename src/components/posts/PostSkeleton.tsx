6757896620b7412cd17c7bb8d3b37b306bda92f5
dy7791 <dy7791@naver.com>
2025-04-07 14:40:47 +0900

    add:: PostSkeleton

---
+++ src/components/posts/PostSkeleton.tsx
@@ -0,0 +1,12 @@
+function PostSkeleton() {
+  return (
+    <div className="flex ml-3 h-70 w-85 justify-center bg-gray-400 rounded-2xl animate-pulse">
+      <div>
+        <div className="mt-3 h-40 w-80 rounded-2xl bg-gray-300"></div>
+        <div className="mt-3 h-20 w-80 rounded-2xl bg-gray-300"></div>
+      </div>
+    </div>
+  );
+}
+
+export default PostSkeleton;
