1f752edac4dff78e8cfeb3e34c2db4a950690ce7
dy7791 <dy7791@naver.com>
2025-04-07 14:40:24 +0900

    add:: PostItem

---
+++ src/components/posts/PostItem.tsx
@@ -0,0 +1,12 @@
+function PostItem() {
+  return (
+    <div className="grid mt-3 grid-cols-4 h-80 w-full border-1 border-red-200 items-center">
+      <div className="ml-3 h-70 w-85 border-1"></div>
+      <div className="ml-3 h-70 w-85 border-1"></div>
+      <div className="ml-3 h-70 w-85 border-1"></div>
+      <div className="ml-3 h-70 w-85 border-1"></div>
+    </div>
+  );
+}
+
+export default PostItem;
