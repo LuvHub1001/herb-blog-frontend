bee3b7f9828b4f9515959900aafa22cf87e0f174
dy7791 <dy7791@naver.com>
2025-04-08 17:23:50 +0900

    fix:: PostList 임시 style 작성

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
 import { get } from "../../apis";
 import { useFetch } from "../../hooks";
-import { PostType } from "../../types";
+import { PostType, Nullable } from "../../types";
 
 interface PostFetchResult {
-  data: PostType;
+  data: Nullable<PostType[]>;
+  totalPageCount: number;
 }
 
 function PostList() {
@@ -14,16 +15,114 @@ function PostList() {
     "http://localhost:5000/api/boards/",
   );
 
-  return (
-    <div className="flex justify-center border-1 border-red-400">
-      <div className="grid grid-cols-3 mt-3 w-[80%] h-250 border-1 text-center items-center">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+  const tilItems = useFetch<string, PostFetchResult>(
+    get,
+    "http://localhost:5000/api/boards/til",
+  );
 
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+  const diaryItems = useFetch<string, PostFetchResult>(
+    get,
+    "http://localhost:5000/api/boards/diary",
+  );
+
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-cols-3 gap-10 mt-3 mb-3 w-[70%] h-180  text-center">
+        <div className="image-container mt-3 h-87 border-1 border-blue-500 rounded-2xl">
+          <div className="flex h-55 justify-center items-center border-b-1">
+            <img alt="이미지" src="zz" />
+          </div>
+
+          <div className="info-container text-left pl-3 pt-3 pr-3">
+            <div className="flex justify-between">
+              <div>임시 제목</div>
+              <div>임시 작성자</div>
+            </div>
+            <div>임시 서브 제목</div>
+            <div>임시 서브 내용</div>
+            <div>임시 작성 일자</div>
+          </div>
+        </div>
+
+        <div className="image-container mt-3 h-87 border-1 border-blue-500 rounded-2xl">
+          <div className="flex h-55 justify-center items-center border-b-1">
+            <img alt="이미지" src="zz" />
+          </div>
+
+          <div className="info-container text-left pl-3 pt-3 pr-3">
+            <div className="flex justify-between">
+              <div>임시 제목</div>
+              <div>임시 작성자</div>
+            </div>
+            <div>임시 서브 제목</div>
+            <div>임시 서브 내용</div>
+            <div>임시 작성 일자</div>
+          </div>
+        </div>
+
+        <div className="image-container mt-3 h-87 border-1 border-blue-500 rounded-2xl">
+          <div className="flex h-55 justify-center items-center border-b-1">
+            <img alt="이미지" src="zz" />
+          </div>
+
+          <div className="info-container text-left pl-3 pt-3 pr-3">
+            <div className="flex justify-between">
+              <div>임시 제목</div>
+              <div>임시 작성자</div>
+            </div>
+            <div>임시 서브 제목</div>
+            <div>임시 서브 내용</div>
+            <div>임시 작성 일자</div>
+          </div>
+        </div>
+
+        <div className="image-container mt-3 h-87 border-1 border-blue-500 rounded-2xl">
+          <div className="flex h-55 justify-center items-center border-b-1">
+            <img alt="이미지" src="zz" />
+          </div>
+
+          <div className="info-container text-left pl-3 pt-3 pr-3">
+            <div className="flex justify-between">
+              <div>임시 제목</div>
+              <div>임시 작성자</div>
+            </div>
+            <div>임시 서브 제목</div>
+            <div>임시 서브 내용</div>
+            <div>임시 작성 일자</div>
+          </div>
+        </div>
+
+        <div className="image-container mt-3 h-87 border-1 border-blue-500 rounded-2xl">
+          <div className="flex h-55 justify-center items-center border-b-1">
+            <img alt="이미지" src="zz" />
+          </div>
+
+          <div className="info-container text-left pl-3 pt-3 pr-3">
+            <div className="flex justify-between">
+              <div>임시 제목</div>
+              <div>임시 작성자</div>
+            </div>
+            <div>임시 서브 제목</div>
+            <div>임시 서브 내용</div>
+            <div>임시 작성 일자</div>
+          </div>
+        </div>
+
+        <div className="image-container mt-3 h-87 border-1 border-blue-500 rounded-2xl">
+          <div className="flex h-55 justify-center items-center border-b-1">
+            <img alt="이미지" src="zz" />
+          </div>
+
+          <div className="info-container text-left pl-3 pt-3 pr-3">
+            <div className="flex justify-between">
+              <div>임시 제목</div>
+              <div>임시 작성자</div>
+            </div>
+            <div>임시 서브 제목</div>
+            <div>임시 서브 내용</div>
+            <div>임시 작성 일자</div>
+          </div>
+        </div>
       </div>
     </div>
   );
