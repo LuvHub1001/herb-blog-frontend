0376a8323cba31e334afb8696b94666872b85021
dy7791 <dy7791@naver.com>
2025-04-08 15:23:30 +0900

    add:: PostPage

---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,11 @@
+import { PostList } from "../components";
+
+function PostPage() {
+  return (
+    <>
+      <PostList />
+    </>
+  );
+}
+
+export default PostPage;
--- src/routes/PublicRouter.tsx
+++ src/routes/PublicRouter.tsx
@@ -6,12 +6,16 @@ import { Loading } from "../components";
 const MainPage = lazy(() => import("../pages/MainPage"));
 const WrappedMainPage = withCommonLayout(MainPage);
 
+const PostPage = lazy(() => import("../pages/PostPage"));
+const WrappedPostPage = withCommonLayout(PostPage);
+
 function PublicRouter() {
   return (
     <Suspense fallback={<Loading />}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<WrappedMainPage />} />
+          <Route path="/posts/:category" element={<WrappedPostPage />} />
         </Routes>
       </BrowserRouter>
     </Suspense>
