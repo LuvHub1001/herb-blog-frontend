e90fb372c6b1efdcd95d656947694fb246d30b32
dy7791 <dy7791@naver.com>
2025-04-08 11:04:07 +0900

    fix:: Footer >> Modal 적용

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,29 @@
+import { useModal } from "../../hooks";
+import { LoginModal } from "../";
+
 function Footer() {
+  const { isOpen, handleClick } = useModal();
+
   return (
-    <div className="flex h-30 mt-30 justify-center items-center bg-black text-white"></div>
+    <>
+      <div className="flex mt-30 h-30 w-full items-center justify-center bg-black text-white">
+        <div>ⓒ 2025. Herb Corp. All rights reserved.</div>
+        <img
+          className="cursor-pointer"
+          src="/images/user_icon.png"
+          onClick={handleClick}
+        />
+      </div>
+
+      {isOpen ? (
+        <div
+          className="flex mt-20 fixed inset-0 items-center justify-center bg-black/50 backdrop-blur-sm"
+          onClick={handleClick}
+        >
+          <LoginModal />
+        </div>
+      ) : null}
+    </>
   );
 }
 
