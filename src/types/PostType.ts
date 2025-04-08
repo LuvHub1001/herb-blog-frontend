3715fb4fea12896650fff3f435ffb3ec798a0be6
dy7791 <dy7791@naver.com>
2025-04-08 14:17:58 +0900

    add:: PostType

---
+++ src/types/PostType.ts
@@ -0,0 +1,11 @@
+export type PostType = {
+  id: number;
+  writer: string;
+  title: string;
+  subTitle: string;
+  content: string;
+  subContent: string;
+  thumbnail: string;
+  category: string;
+  workdate: string;
+};
