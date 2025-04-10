b005f28c03f4ea86815d28f26385ae9cab5a54d3
dy7791 <dy7791@naver.com>
2025-04-10 14:54:14 +0900

    add:: create Post DB 연동

--- src/components/admin/PostEditor.tsx
+++ src/components/admin/PostEditor.tsx
@@ -1,13 +1,51 @@
+import { useRef, useState } from "react";
 import "@toast-ui/editor/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
+import { post } from "../../apis";
 
 function PostEditor() {
+  const editorRef = useRef<Editor>(null);
+  const [formData, setFormData] = useState({
+    category: "",
+    title: "",
+    subTitle: "",
+    writer: "LuvHub",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSave = async () => {
+    const editorInstance = editorRef.current;
+    const content = editorInstance?.getInstance().getMarkdown() || "";
+
+    const postData = {
+      ...formData,
+      content,
+    };
+
+    try {
+      post("http://localhost:5000/api/boards", postData);
+      alert("게시글이 저장되었습니다!");
+      window.location.href = "/";
+    } catch (err) {
+      alert("게시글 저장에 실패했습니다.");
+    }
+  };
+
   return (
-    <form action="post">
-      <div className="title-container flex mt-10 h-45 w-[1500px] m-auto border border-gray-200 rounded-2xl items-center">
+    <form action="post" onSubmit={(e) => e.preventDefault()}>
+      <div className="title-container flex mt-10 h-55 w-[1500px] m-auto border border-gray-200 rounded-2xl items-center">
         <div className="w-full">
           <div className="category-box flex ml-7 h-13 w-[500px] border border-gray-200 rounded-[8px]">
-            <select className="w-[500px] pl-1">
+            <select
+              name="category"
+              className="w-[500px] pl-1"
+              onChange={handleChange}
+            >
               <option defaultValue="">카테고리를 선택해 주세요.</option>
               <option value="til">TIL</option>
               <option value="diary">Diary</option>
@@ -15,8 +53,18 @@ function PostEditor() {
           </div>
           <div className="title-box flex mt-3 pl-7 pr-7">
             <input
-              placeholder="제목을 입력해 주세요."
+              name="subTitle"
+              className="bg-gray-200 w-1/2 h-13 text-[18px] rounded-[8px] placeholder: pl-3"
+              placeholder="소제목을 입력해 주세요."
+              onChange={handleChange}
+            />
+          </div>
+          <div className="title-box flex mt-3 pl-7 pr-7">
+            <input
+              name="title"
               className="bg-gray-200 w-full h-13 text-[18px] rounded-[8px] placeholder: pl-3"
+              placeholder="제목을 입력해 주세요."
+              onChange={handleChange}
             />
           </div>
         </div>
@@ -24,6 +72,7 @@ function PostEditor() {
 
       <div className="editor-container w-[1500px] mt-5 mx-auto ">
         <Editor
+          ref={editorRef}
           initialValue="내용을 입력하세요"
           previewStyle="vertical"
           height="800px"
@@ -36,6 +85,7 @@ function PostEditor() {
         <button
           type="button"
           className="w-20 h-10 bg-blue-300 rounded-[8px] cursor-pointer"
+          onClick={handleSave}
         >
           저장
         </button>
