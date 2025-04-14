0a76af57055cbee8d80af9c288437cb8b0ec2d85
dy7791 <dy7791@naver.com>
2025-04-14 10:49:30 +0900

    fix:: 글자 수에 따른 제목 처리 및 스타일 수정, 마크다운 regex 적용

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { PostType } from "../../types";
 
 interface itemsType {
@@ -5,21 +6,55 @@ interface itemsType {
 }
 
 function PostItem({ items }: itemsType) {
+  const navigate = useNavigate();
+
+  const markdownRegex = (markdown: string) => {
+    return markdown
+      .replace(/!\[.*?\]\(.*?\)/g, "") // 이미지 제거
+      .replace(/\[.*?\]\(.*?\)/g, "") // 링크 제거
+      .replace(/[#>*_\-\+~`]/g, "") // 특수문자 제거
+      .replace(/\n+/g, " ") // 줄바꿈 -> 공백
+      .trim();
+  };
+
   return (
     <>
-      <div className="mt-3 h-80 border-1">
-        <div className="image-container flex w-full h-60 border-b-1">
+      <div className="mt-3 h-85 w-90 border-1 rounded-2xl">
+        <div className="image-container flex w-full h-50 border-b-1">
           <img
             src={items.thumbnail || "/images/default-thumbnail.png"}
-            className="w-full h-full border-0 cursor-pointer"
+            className="w-full h-full cursor-pointer rounded-t-2xl"
+            onClick={() => navigate(`/posts/detail/${items.id}`)}
           />
         </div>
-        <div className="flex flex-col gap-2">
-          <div className="pl-3 pt-3">
-            <span className="cursor-pointer">{items.title}</span>
+        <div className="pt-2 flex flex-col">
+          <div>
+            <span
+              className="pl-2 text-[20px] font-bold cursor-pointer"
+              onClick={() => navigate(`/posts/detail/${items.id}`)}
+            >
+              {items.title.length > 15
+                ? items.title.slice(0, 22) + "..."
+                : items.title}
+            </span>
           </div>
-          <div className="pl-3">
-            <span className="cursor-pointer">{items.subTitle}</span>
+          <div className="flex justify-between pt-2 pl-2">
+            <span
+              className="cursor-pointer text-gray-700"
+              onClick={() => navigate(`/posts/detail/${items.id}`)}
+            >
+              {items.subTitle}
+            </span>
+            <span className="pr-1">{items.workdate.slice(0, 10)}</span>
+          </div>
+
+          <div
+            className="pt-2 pl-2"
+            onClick={() => navigate(`/posts/detail/${items.id}`)}
+          >
+            {markdownRegex(items.content).length > 50
+              ? markdownRegex(items.content).slice(0, 50) + "..."
+              : markdownRegex(items.content)}
           </div>
         </div>
       </div>
