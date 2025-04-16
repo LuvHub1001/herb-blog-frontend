5c8ff95d78731a7ae70c7ca168b5986704552437
dy7791 <dy7791@naver.com>
2025-04-16 16:06:31 +0900

    관리자 버튼 위치 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Navibar } from "..";
 
 function Header() {
+  const navigate = useNavigate();
+  const locate = useLocation();
+  const [isAdmin, setIsAdmin] = useState<boolean>(true);
+
   return (
     <>
       <Navibar />
-      <div className="bg-header flex h-40 items-center justify-center text-white"></div>
+      <div className="bg-header flex h-40 items-center justify-center text-white text-left">
+        {isAdmin && locate.pathname !== "/admin" ? (
+          <button className="cursor-pointer" onClick={() => navigate("/admin")}>
+            관리자
+          </button>
+        ) : null}
+      </div>
     </>
   );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,7 @@
-import { useNavigate } from "react-router-dom";
 import { PostContainer } from "../components";
 
 function MainPage() {
-  const navigate = useNavigate();
-  return (
-    <>
-      <button onClick={() => navigate("/posts/edit")}>글쓰기</button>
-      <PostContainer />
-    </>
-  );
+  return <PostContainer />;
 }
 
 export default MainPage;
