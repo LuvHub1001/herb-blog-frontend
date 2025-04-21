3a95df76cd8fe15a9276c15cc625ac866cf4e3f6
dy7791 <dy7791@naver.com>
2025-04-21 14:41:36 +0900

    fix:: token 없으면 접근 못하게

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { AdminChart } from "../components";
 
 function AdminPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    if (!token) {
+      navigate("/");
+    }
+  }, [navigate]);
+
   return <AdminChart />;
 }
 
