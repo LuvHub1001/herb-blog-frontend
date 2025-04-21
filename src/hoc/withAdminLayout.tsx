04844af03332cf96859494fed3494f1d01d3ff3c
dy7791 <dy7791@naver.com>
2025-04-21 18:49:20 +0900

    fix:: overflow

--- src/hoc/withAdminLayout.tsx
+++ src/hoc/withAdminLayout.tsx
@@ -4,7 +4,7 @@ import { Header, Footer, Sidebar } from "../components";
 function withCommonLayout(Component: ComponentType) {
   const WrappedComponent = () => {
     return (
-      <div className="relative">
+      <div className="relative overflow-hidden">
         <Header />
         <div className="flex min-h-[calc(100vh-360px)]">
           <Sidebar />
